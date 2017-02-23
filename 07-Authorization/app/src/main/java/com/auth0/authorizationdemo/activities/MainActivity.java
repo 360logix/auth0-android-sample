1c081ea6e2060d98fe6dcae37166da4778f74db9
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-02-23 17:11:03 -0300

    update the Authorization sample to work with /oauth/token

--- 07-Authorization/app/src/main/java/com/auth0/authorizationdemo/activities/MainActivity.java
+++ 07-Authorization/app/src/main/java/com/auth0/authorizationdemo/activities/MainActivity.java
@@ -5,25 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.auth0.android.Auth0;
-import com.auth0.android.authentication.AuthenticationAPIClient;
-import com.auth0.android.authentication.AuthenticationException;
-import com.auth0.android.callback.BaseCallback;
-import com.auth0.android.management.ManagementException;
-import com.auth0.android.management.UsersAPIClient;
-import com.auth0.android.result.UserProfile;
+import com.auth0.android.jwt.JWT;
 import com.auth0.authorizationdemo.R;
 import com.auth0.authorizationdemo.utils.CredentialsManager;
-import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private UserProfile mUserProfile;
+    private String userRole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,46 +30,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
-        auth0.setOIDCConformant(true);
-        AuthenticationAPIClient authenticationClient = new AuthenticationAPIClient(auth0);
-        authenticationClient.userInfo(CredentialsManager.getCredentials(this).getAccessToken())
-                .start(new BaseCallback<UserProfile, AuthenticationException>() {
-                    @Override
-                    public void onSuccess(final UserProfile info) {
-                        String userId = (String) info.getExtraInfo().get("sub");
-                        UsersAPIClient usersClient = new UsersAPIClient(auth0, CredentialsManager.getCredentials(MainActivity.this).getIdToken());
-                        usersClient.getProfile(userId).start(new BaseCallback<UserProfile, ManagementException>() {
-                            @Override
-                            public void onSuccess(final UserProfile profile) {
-                                runOnUiThread(new Runnable() {
-                                    public void run() {
-                                        mUserProfile = profile;
-                                        ((TextView) findViewById(R.id.userName)).setText(mUserProfile.getName());
-                                        ((TextView) findViewById(R.id.userEmail)).setText(mUserProfile.getEmail());
-                                        ImageView userPicture = (ImageView) findViewById(R.id.userPicture);
-                                        Picasso.with(MainActivity.this).load(mUserProfile.getPictureURL()).into(userPicture);
-                                    }
-                                });
-                            }
-
-                            @Override
-                            public void onFailure(ManagementException error) {
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(AuthenticationException error) {
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-                                Toast.makeText(MainActivity.this, "Failed to load the profile", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        finish();
-                    }
-                });
-
         Button showSettingsButton = (Button) findViewById(R.id.toSettingsButton);
         showSettingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,23 +38,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final JWT jwt = new JWT(CredentialsManager.getCredentials(this).getIdToken());
+        String roleClaim = "https://access.control/role";
+        userRole = jwt.getClaim(roleClaim).asString();
+
+        TextView roleText = (TextView) findViewById(R.id.userRole);
+        roleText.setText(String.format("Your role: %s", userRole));
     }
 
     private void showSettings() {
-        String[] roles = (String[]) mUserProfile.getAppMetadata().get("roles");
-        if (roles == null) {
-            Toast.makeText(MainActivity.this, "Missing roles from the Profile. Check the rules in the dashboard.", Toast.LENGTH_LONG).show();
+        if (!"admin".equals(userRole)) {
+            Toast.makeText(MainActivity.this, "You don't have access rights to visit this page", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        for (String role : roles) {
-            if (role.contains("admin")) {
-                startActivity(new Intent(this, SettingsActivity.class));
-                return;
-            }
-        }
-
-        Toast.makeText(MainActivity.this, "You don't have access rights to visit this page", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 
     private void loginAgain() {
